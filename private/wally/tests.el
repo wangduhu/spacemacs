c157f57e89f0400dc635950512e39ea39afa2315
wally <buaaben@163.com>
2022-10-05 13:33:16 +0800

    feat: 增加scramy接口，判断是否访问过指定纪录

--- private/wally/tests.el
+++ private/wally/tests.el
@@ -1,16 +1,20 @@
 (ert-deftest example-test ()
   (should (equal (+ 9 2) 11)))
 
-(ert-deftest test:_wally/org-norm-value-property ()
+(ert-deftest test:wally/org-norm-value-property ()
   (should (= (_wally/org-norm-value-property "30") 30))
   (should (= (_wally/org-norm-value-property "00:00") 0))
   (should (= (_wally/org-norm-value-property "00.45") 45))
   (should (= (_wally/org-norm-value-property "23.30") -30))
   )
 
-(ert-deftest test:_wally/anki-is-note-existed ()
+(ert-deftest test:wally/anki-is-note-existed ()
   (should (= (wally/anki-is-note-existed "bose qc35") 1651384178161))
   (should-not (wally/anki-is-note-existed "bose qc53")))
 
+(ert-deftest test:wally/logb-has-scramy-item-visited ()
+  (should (wally/logb-has-scramy-item-visited "netbian" "/tupian/29521.html"))
+  (should-not (wally/logb-has-scramy-item-visited "foo" "bar"))
+  )
 
 (ert t)
